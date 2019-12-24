feabbff06e6628b9a3cac189961cb033596a21b3
iskwhdys <iskwhdys@gmail.com>
2019-12-25 00:18:51 +0900

    Study-OnlineStoreOrderとのやり取りの実装を開始

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="field">
+    <h3>検索条件</h3>
+    <div>
+      <label>Search</label>
+      <input v-model="searchText" placeholder="text" type="text" />
+      <button @click="onClickSearch">Search</button>
+    </div>
+    
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+@Component
+export default class Home extends Vue {
+
+  // axios用の接続先
+  dbHost: string = "http://localhost:8081/api/";
+
+  // 入力された検索ワード
+  searchText: string = "";
+
+  created() {
+    console.log("created");
+  }
+
+  // 検索ボタンの押下
+  async onClickSearch() {
+    console.info("onClickSearch-start");
+    const data = await this.getAllInventoryData(this.searchText);
+    console.info(data);
+    console.info("onClickSearch-end");
+  }
+
+  async getAllInventoryData(searchText: string) {
+    // TODO:CORSの対応をしないと別オリジン(≒ドメイン)へのアクセスが出来ない
+    const response = await require("axios").get(this.dbHost + "inventory", {
+
+    });
+    return response.data;
   }
 }
 </script>
+
+<style lang="scss">
+#field {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+</style>
