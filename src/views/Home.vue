5abe0fb51227804e46b312ef7da800a5062ff786
iskwhdys <iskwhdys@gmail.com>
2019-12-30 00:11:15 +0900

    取得したインベントリデータの仮表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="field">
-    <h3>検索条件</h3>
+
+    <!-- <h3>検索条件</h3>
     <div>
       <label>Search</label>
       <input v-model="searchText" placeholder="text" type="text" />
       <button @click="onClickSearch">Search</button>
-    </div>
-    
+    </div> -->
+
     <router-view></router-view>
+    <button @click="onGetData">データ取得</button>
+    <div v-for="item in items" :key="item.productCode + item.inventoryNumber">
+      {{ item.productCode }}：{{ item.inventoryNumber }}：{{ item.quantity }}
+    </div>
   </div>
 </template>
 
@@ -16,29 +21,34 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Home extends Vue {
-
   // axios用の接続先
   dbHost: string = "http://localhost:8081/api/";
-
   // 入力された検索ワード
   searchText: string = "";
+  // 
+  items: any = "";
 
   created() {
     console.log("created");
   }
 
+  async onGetData() {
+    console.info("onGetData-start");
+    this.items = await this.getAllInventoryData(this.searchText);
+    console.info(this.items);
+    console.info("onGetData-end");
+  }
+
   // 検索ボタンの押下
   async onClickSearch() {
     console.info("onClickSearch-start");
-    const data = await this.getAllInventoryData(this.searchText);
-    console.info(data);
+
+
     console.info("onClickSearch-end");
   }
 
   async getAllInventoryData(searchText: string) {
-    const response = await require("axios").get(this.dbHost + "inventory", {
-
-    });
+    const response = await require("axios").get(this.dbHost + "inventory", {});
     return response.data;
   }
 }
